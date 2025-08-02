84aaa8b53b7931e9c0ad88a7273ece0453dc75fc
gupi debnath <iamgupidebnath@gmail.com>
2025-08-02 23:16:24 +0530

    Add product categories with images and titles

---
+++ src/data/productCategory.js
@@ -0,0 +1,66 @@
+import airPods from "../assets/images/air-pods.png";
+import airTags from "../assets/images/air-tags.png";
+import computer from "../assets/images/computer.png";
+import homePads from "../assets/images/home-pads.png";
+import macMini from "../assets/images/mac-mini.png";
+import mobileCover from "../assets/images/mobile-cover.png";
+import mobiles from "../assets/images/mobile.png";
+import tabs from "../assets/images/tabs.png";
+import watch from "../assets/images/watch.png";
+
+export const productCategory = [
+  {
+    id: 1,
+    title: "Mac",
+    slug: "mac",
+    image: computer,
+  },
+  {
+    id: 2,
+    title: "iPhone",
+    slug: "iphone",
+    image: mobiles,
+  },
+  {
+    id: 3,
+    title: "iPad",
+    slug: "ipad",
+    image: tabs,
+  },
+  {
+    id: 4,
+    title: "Apple Watch",
+    slug: "watch",
+    image: watch,
+  },
+  {
+    id: 5,
+    title: "AirPods",
+    slug: "airpods",
+    image: airPods,
+  },
+  {
+    id: 6,
+    title: "AirTag",
+    slug: "airtag",
+    image: airTags,
+  },
+  {
+    id: 7,
+    title: "Apple TV 4K",
+    slug: "tv-4k",
+    image: macMini,
+  },
+  {
+    id: 8,
+    title: "HomePod",
+    slug: "homepod",
+    image: homePads,
+  },
+  {
+    id: 9,
+    title: "Accessories",
+    slug: "accessories",
+    image: mobileCover,
+  },
+];
