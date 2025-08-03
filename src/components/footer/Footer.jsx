d7d536c707609578d938339cb72dd447e281f52d
BikramDeb10 <bikramdebnath8926@gmail.com>
2025-08-03 09:36:41 +0530

    setup project

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        background: "#222",
+        color: "#fff",
+        padding: "1rem",
+        textAlign: "center",
+      }}
+    >
+      Footer Content © 2025
+    </footer>
+  );
+};
+
+export default Footer;
